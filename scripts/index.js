36a8153d006021e8236e4626ed677cb1cc66e0a3
Alejandro Regus <Aregus@appdev.yearup.org>
2023-06-09 15:25:46 -0400

    dropdown added-working on dropdown for categories

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,16 @@
+const searchProducgtEl = document.getElementById(`searchSelector`);
+console.log(searchselectorEl)
 fetch(`http://localhost:8081/api/categories`)
-.then((response) => response.json())
-.then((data) => { 
-    console.log(data);
-})
+  .then((response) => response.json())
+  .then((categories) => {
+    for (i = 0; i < categories.length; i++) {
+      let optionel = document.createElement("option");
+      const categoryArray = categories[i];
+      optionel.textContent = categoryArray.name;
+      searchselectorEl.appendChild(optionel);
+    }
+  });
+
+//   searchselectorEl.addEventListener(`change`, () => {
+//     let selectedValue = 
+//   })
